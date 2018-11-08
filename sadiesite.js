c87971bc98ef41caafe3f255eec623b000056935
Sadie Collins <sadiecollins13@gmail.com>
2018-11-08 13:16:41 -0600

    added hamburger menu

---
+++ sadiesite.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $('.menu-trigger').click(function() {
+    $('.container').slideToggle(500);
+  });
+
+
+  $(window).resize(function() {
+	   if (  $(window).width() > 700 ) {
+		$('.container').removeAttr('style');
+	 }
+	});
+});
